79c1afbc4f473ec06415f603d5918770e0503b45
Alonso-pixel <kreitos_05@outlook.com>
2024-05-01 11:57:39 -0600

    "origin"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sabo Motors",
   description: "Sabo Motors",
+
+  other: {},
 };
 
 export default function RootLayout({
@@ -16,6 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Your Page Title</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
