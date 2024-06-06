20b30d07aa8861f3350bea3c2be9ace111b62519
Alonso-pixel <kreitos_05@outlook.com>
2024-06-06 12:15:35 -0600

    added buttons form and stories

--- src/components/vehicle-display.tsx
+++ src/components/vehicle-display.tsx
@@ -1,39 +1,6 @@
-// import { AvailableCars } from "@prisma/client";
-// import VehicleSection from "./vehicle-section";
-
-// function VehicleDisplay({
-//   vehicles,
-//   currentCarType,
-//   currentPrice,
-//   currentFinancing,
-// }: any) {
-//   const initialState =
-//     currentCarType === "all" &&
-//     currentPrice === "~~~~" &&
-//     currentFinancing === "all";
-//   return vehicles.map(
-//     (vehicle: AvailableCars) =>
-//       (initialState ||
-//         (vehicle.price === currentPrice &&
-//           vehicle.credit === currentFinancing)) && (
-//         <VehicleSection
-//           key={vehicle.id}
-//           id={vehicle.id}
-//           imageSrc={vehicle.image}
-//           downpayment={vehicle.downpayment}
-//           model={vehicle.model}
-//           credit={vehicle.credit}
-//           features={vehicle.features}
-//           price={vehicle.price}
-//         />
-//       )
-//   );
-// }
-
 import { useContext } from "react";
 import { FilterContext } from "../context/filter-context";
 import VehicleSection from "./vehicle-section";
-import { vehicles } from "@/utils/data";
 
 function VehicleDisplay({ cars }: { cars: any[] }) {
   const { budget, vehicleType, financing } = useContext(FilterContext);
